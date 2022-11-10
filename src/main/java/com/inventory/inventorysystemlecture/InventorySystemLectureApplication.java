2ccc08d1117cb9297a681a836cee11a16d2b4f70
hyuk <pjh08190819@naver.com>
2022-11-11 01:37:53 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/inventory/inventorysystemlecture/InventorySystemLectureApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.inventorysystemlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventorySystemLectureApplication.class, args);
+    }
+
+}
