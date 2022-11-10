b17a2ed729b5b9809fd77ee3bbc219e604e75627
hyuk <pjh08190819@naver.com>
2022-11-11 02:30:15 +0900

    study : 비관적 락 테스트

--- src/main/java/com/inventory/inventorysystemlecture/repository/StockRepository.java
+++ src/main/java/com/inventory/inventorysystemlecture/repository/StockRepository.java
@@ -7,4 +7,9 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
+
 }
